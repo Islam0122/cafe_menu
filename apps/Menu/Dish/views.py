d55f2b14cc629007d9b9ab806caede9762468540
islam <dujsobaevislam01@gmail.com>
2024-02-18 03:25:28 +0600

    search/filter -> dish

--- apps/Menu/Dish/views.py
+++ apps/Menu/Dish/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from . import models
@@ -14,9 +15,25 @@ class DishViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.DishSerializer
     lookup_field = 'pk'
 
+
 def DishListView(request):
-    dish = models.Dish.objects.all()
-    return render(request, 'menu/dish/dish.html', {'dishes': dish})
+    if request.method == 'GET':
+        dishes = models.Dish.objects.all()
+        categories = models.Category.objects.all()
+        search_text = request.GET.get("search")
+        category_filter = request.GET.get("category")
+
+        if search_text:
+            dishes = dishes.filter(
+                Q(name__icontains=search_text) |
+                Q(price__contains=search_text)
+            )
+        if category_filter:
+            dishes = dishes.filter(category__title=category_filter)
+
+    return render(request, 'menu/dish/dish.html', {'dishes': dishes,'categories': categories})
+
+
 def DishDetailView(request, pk):
-    dish= get_object_or_404(models.Dish, pk=pk)
+    dish = get_object_or_404(models.Dish, pk=pk)
     return render(request, 'menu/dish/dish_detail.html', {'dish': dish})
